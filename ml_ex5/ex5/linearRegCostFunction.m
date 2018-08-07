3f48b0f0051bbc936272aec60608128694c5b505
Garrett Ransom <g2ransom@gmail.com>
2018-08-07 09:12:18 -0400

    changed gradient logic into a vectorized approach and created temp and h variables

--- ml_ex5/ex5/linearRegCostFunction.m
+++ ml_ex5/ex5/linearRegCostFunction.m
@@ -19,11 +19,11 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
-
-J = (1/(2*m)) * sum((((X*theta) - y).^2)) + (lambda / (2*m)) * sum(theta(2:end).^2);
-% g0 = (1/m) * sum((X(:, 1)*theta) - y) * X(:, 1);
-% g1 = (1/m) * sum(((X(:, 2)*theta) - y)' * X(:, 2)) + (lambda/ m) * theta(2:end);
-% grad = [g0 ; g1];
+h = X*theta;
+J = (1/(2*m)) * sum(((h - y).^2)) + (lambda / (2*m)) * sum(theta(2:end).^2);
+temp=theta;
+temp(1)=0;
+grad=(1/m).*(X'*(h-y)+lambda.*temp);
 
 
 
