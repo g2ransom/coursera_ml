169d222da6da0fd05ba575c865c33ac819e5489f
Garrett Ransom <g2ransom@gmail.com>
2018-08-08 08:30:31 -0400

    added function to create a matrix of polynomial features

--- ml_ex5/ex5/polyFeatures.m
+++ ml_ex5/ex5/polyFeatures.m
@@ -14,7 +14,11 @@ X_poly = zeros(numel(X), p);
 %               column of X contains the values of X to the p-th power.
 %
 % 
-
+m = length(X);
+for i = 1:m
+	for j = 1:p
+		X_poly(i, j) = X(i).^j;
+end
 
 
 
